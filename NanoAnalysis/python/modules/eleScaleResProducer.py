20b153f359c7a079ec8a1005a63eb4ae73cfa1bb
Martina Manoni <m.manoni2@campus.unimib.it>
2025-03-29 17:42:51 +0100

    Update Ele scale and smearing correction to be Et dependent + small fixes in csv files

--- NanoAnalysis/python/modules/eleScaleResProducer.py
+++ NanoAnalysis/python/modules/eleScaleResProducer.py
@@ -1,7 +1,7 @@
 import os
 
 # Set up the NATModules eleScaleRes module
-def getEleScaleRes(era, tag, is_mc, overwritePt=True) :
+def getEleScaleRes(era, tag, is_mc, overwritePt=True, EtDependent=True) :
     from PhysicsTools.NATModules.modules.eleScaleRes import eleScaleRes
 
     # Check for supported eras
@@ -9,25 +9,26 @@ def getEleScaleRes(era, tag, is_mc, overwritePt=True) :
         raise ValueError(f"getEleScaleRes: Era {era} not supported")
 
     if era == 2022:
-        scaleKey = "Scale"
-        smearKey = "Smearing" if is_mc else None
-
         if "pre_EE" in tag :
-            fname = "electronSS_preEE.json.gz"
+            scaleKey = "EGMScale_Compound_Ele_2022preEE"
+            smearKey = "EGMSmearAndSyst_ElePTsplit_2022preEE" if is_mc else None
+            fname = "electronSS_EtDependent_2022preEE.json.gz"
         else:
-            fname = "electronSS_postEE.json.gz"
+            scaleKey = "EGMScale_Compound_Ele_2022postEE"
+            smearKey = "EGMSmearAndSyst_ElePTsplit_2022postEE" if is_mc else None
+            fname = "electronSS_EtDependent_2022postEE.json.gz"
 
     elif era == 2023:
         if "pre_BPix" in tag:
-            scaleKey = "2023PromptC_ScaleJSON"
-            smearKey = "2023PromptC_SmearingJSON" if is_mc else None
-            fname = "electronSS_preBPix.json.gz"
+            scaleKey = "EGMScale_Compound_Ele_2023preBPIX"
+            smearKey = "EGMSmearAndSyst_ElePTsplit_2023preBPIX" if is_mc else None
+            fname = "electronSS_EtDependent_2023preBPix.json.gz"
         else:
-            scaleKey = "2023PromptD_ScaleJSON"
-            smearKey = "2023PromptD_SmearingJSON" if is_mc else None
-            fname = "electronSS_postBPix.json.gz"
+            scaleKey = "EGMScale_Compound_Ele_2023postBPIX"
+            smearKey = "EGMSmearAndSyst_ElePTsplit_2023postBPIX" if is_mc else None
+            fname = "electronSS_EtDependent_2023postBPix.json.gz"
  
     json = "%s/src/ZZAnalysis/NanoAnalysis/data/%s" % (os.environ['CMSSW_BASE'], fname)
 
     print("***eleScaleRes: era:", era, "tag:", tag, "is MC:", is_mc, "overwritePt:", overwritePt, "json:", json)
-    return eleScaleRes(json, scaleKey, smearKey, overwritePt)
+    return eleScaleRes(json, scaleKey, smearKey, overwritePt, EtDependent)
